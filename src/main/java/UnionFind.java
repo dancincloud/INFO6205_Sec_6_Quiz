3841d96a2754b2a10a759259eea111db1c046789
Joseph Yuanhao Li <Josephl.b612@gmail.com>
2021-10-05 17:02:23 -0400

    add quiz_3

---
+++ src/main/java/UnionFind.java
@@ -0,0 +1,7 @@
+/**
+ * @author: Joseph Li @ Dancincloud
+ * Date: 9/29/21 00:09
+**/package PACKAGE_NAME;
+public class UnionFind {
+    public static void main(String[] args){}
+}
---
+++ src/test/java/UnionFindTest.java
@@ -0,0 +1,7 @@
+/**
+ * @author: Joseph Li @ Dancincloud
+ * Date: 9/29/21 00:25
+**/package PACKAGE_NAME;
+public class UnionFindTest {
+    public static void main(String[] args){}
+}
