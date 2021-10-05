d7c869736c53602a9993e92118a3745c323fe3a5
Joseph Yuanhao Li <Josephl.b612@gmail.com>
2021-10-05 17:03:58 -0400

    add quiz_3

--- src/main/java/UnionFind.java
+++ src/main/java/UnionFind.java
@@ -1,7 +1,42 @@
 /**
  * @author: Joseph Li @ Dancincloud
  * Date: 9/29/21 00:09
-**/package PACKAGE_NAME;
+ **/
+
 public class UnionFind {
-    public static void main(String[] args){}
+    private int[] parent;
+
+    public UnionFind (int N) {
+        parent = new int[N];
+        for (int i = 0; i < N; i++) {
+            parent[i] = i;
+        }
+    }
+
+    // check parent pointer until reach root
+    private int find(int i) {
+        while (i != parent[i]) {
+            parent[i] = parent[parent[i]]; // path compression
+            i = parent[i];
+        }
+
+        return i;
+    }
+
+    void union (int p, int q) {
+        int i = find(p);
+        int j = find(q);
+
+        parent[i] = j;
+    }
+
+    boolean connected (int p, int q) {
+        return find(p) == find(q);
+    }
+
+    void print () {
+        for (int i = 0; i < parent.length; i++) {
+            System.out.printf("%d = %d\n", i, parent[i]);
+        }
+    }
 }
--- src/test/java/UnionFindTest.java
+++ src/test/java/UnionFindTest.java
@@ -1,7 +1,82 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author: Joseph Li @ Dancincloud
  * Date: 9/29/21 00:25
-**/package PACKAGE_NAME;
+ **/
+
 public class UnionFindTest {
-    public static void main(String[] args){}
+    @Test
+    public void test1(){
+        UnionFind uf = new UnionFind(10);
+
+        uf.union(2, 2);
+
+        Assert.assertTrue(uf.connected(2, 2));
+        Assert.assertFalse(uf.connected(4, 5));
+    }
+
+    @Test
+    public void test2(){
+        UnionFind uf = new UnionFind(100);
+
+        uf.union(1, 99);
+
+        Assert.assertTrue(uf.connected(1, 99));
+    }
+
+    @Test
+    public void test3(){
+        UnionFind uf = new UnionFind(1000);
+
+        uf.union(1, 990);
+        uf.union(1, 501);
+
+        Assert.assertTrue(uf.connected(501, 990));
+    }
+
+    @Test
+    public void test4(){
+        UnionFind uf = new UnionFind(100);
+
+        uf.union(2, 83);
+        uf.union(7, 2);
+
+        Assert.assertTrue(uf.connected(7, 83));
+        Assert.assertTrue(uf.connected(2, 7));
+        Assert.assertFalse(uf.connected(2, 82));
+    }
+
+    @Test
+    public void test5(){
+        int n = 100;
+        UnionFind uf = new UnionFind(n);
+        uf.union(2, 83);
+        uf.union(99, 27);
+        uf.union(8, 2);
+        uf.union(99, 8);
+
+        Assert.assertTrue(uf.connected(99, 83));
+        Assert.assertTrue(uf.connected(2, 27));
+
+        uf.union(81, 23);
+        uf.union(21,9);
+        uf.union(11,7);
+        uf.union(23, 9);
+        uf.union(11, 81);
+
+        Assert.assertTrue(uf.connected(81, 7));
+        Assert.assertTrue(uf.connected(11, 21));
+
+        Assert.assertFalse(uf.connected(81, 99));
+        Assert.assertFalse(uf.connected(7, 8));
+
+        Assert.assertFalse(uf.connected(11, 1));
+
+        uf.union(11, 8);
+
+        Assert.assertTrue(uf.connected(81, 99));
+        Assert.assertTrue(uf.connected(7, 8));
+    }
 }
